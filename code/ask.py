9de011b8273aef931e45c911020d3fe1b8edd967
Histefanhere <stefan.zdra@gmail.com>
2018-12-04 23:41:49 +1300

    Decancerified Asking Questions Now, it can be done *very* simply via the new ask.ask() function, which returns the answer! Examples of ask.ask() in action are in the code.
    Also did acouple more things like remove `print(token)` :(
    
    Signed-off-by: Histefanhere <stefan.zdra@gmail.com>

--- code/ask.py
+++ code/ask.py
@@ -1,30 +1,97 @@
-import datetime
-
-import discord
+## no.
+# import datetime, asyncio, discord
+## yes.
+import time, asyncio, discord
 
 colourAsked = 0x00ff00
 colourAnswered = 0xf4d85a
 colourTimedOut = 0xddbb8b
 
-class question:
-    def __init__(self,message,question,reactions = ['👍', '👎']):
-        self.message = None
+# this variable and function let ask.py have access to the client
+client = None
+def init(Client):
+    global client
+    client = Client
 
-        self.originalMessage = message
-        self.question = question
-        self.reactions = reactions
+# stores all question objects currently waiting to be answered
+questions = []
+class Question:
+    def __init__(self, origMessage, content, answers, timeout):
+        self.origMessage = origMessage
+        self.content = content
+        self.answers = answers
+        self.embed = self.getEmbed()
+        # time when question was sent (current time)
+        self.time = time.time()
+
+        # ends up being True if the user answered or False if it timed out.
+        self.answered = None
+        # the emoji the user reacts with, should they answer.
+        self.emoji = None
+        # will end up being answers.get(emoji)
+        self.answer = None
+
 
     def getEmbed(self):
-        embed = discord.Embed(title = "**"+self.question+"**", color = colourAsked)
+        embed = discord.Embed(title = "**"+self.content+"**", color = colourAsked)
         return embed
 
     def setMessage(self,message):
         self.message = message
 
     def getAnsweredEmbed(self):
-        embed = discord.Embed(title = "~~"+self.question+"~~", color = colourAnswered)
+        embed = discord.Embed(title = "~~"+self.content+"~~", color = colourAnswered)
         return embed
 
     def getTimedOutEmbed(self):
-        embed = discord.Embed(title = "~~"+self.question+"~~", color = colourTimedOut)
+        embed = discord.Embed(title = "~~"+self.content+"~~", color = colourTimedOut)
         return embed
+
+    def getAnsweredReply(self):
+        reply = self.origMessage.author.name + " answered with: " + self.emoji
+        return reply
+
+    def getTimedOutReply(self):
+        reply = self.origMessage.author.name + " took too long to answer."
+        return reply
+
+# origMessage: the message that triggered the question (used for channel and author)
+# content: the question that'll be asked
+# OPTIONAL:
+# answers: a dictionary whos keys are the emojis (characters) and values their returned values (any data type)
+#     - MAKE SURE THAT `NONE` IS NEVER A POSSIBLE ANSWER, since this is what the function returns if it times out
+# timeout: the duration of the question
+async def ask(origMessage, content, *, answers={'👍':True,'👎':False}, timeout=30):
+    # these 2 lines make the whole function MUCH more readable :)
+    channel = origMessage.channel
+    author = origMessage.author
+    # generate the quesition object fully and send the message object
+    question = Question(origMessage, content, answers, timeout)
+    question.message = await client.send_message(channel, author.mention, embed=question.embed)
+    questions.append(question)
+
+    # make the bot react with the necceccary reactions
+    for emoji in answers:
+        await client.add_reaction(question.message, emoji)
+
+    while True:
+        if question.answered:
+            # question has been answered
+            await client.edit_message(question.message, question.getAnsweredReply(), embed=question.getAnsweredEmbed())
+            break
+        if time.time() >= question.time + timeout:
+            # question has been timedout (how do you spell this!?!?)
+            question.answer = None
+            question.answered = False
+            await client.edit_message(question.message, question.getTimedOutReply(), embed=question.getTimedOutEmbed())
+            break
+        # this lets other async functions still run while this is waiting for a reaction
+        await asyncio.sleep(1.0)
+
+    # do the final touches, which include clearing reactions and deleating the question object
+    await client.clear_reactions(question.message)
+    questions.remove(question)
+    # return the given answer
+    return question.answer
+
+
--- code/main.py
+++ code/main.py
@@ -1,25 +1,11 @@
-import datetime
-
-import discord
-import asyncio
-
+import datetime, discord, asyncio
 import ask
 
 client = discord.Client()
 
-currentQuestions = [] #questions that still need to be answered
-unassignedQuestions = [] #questions that haven't been paired with a message yet (stoof boi I command you to decancerify this)
-
-
-async def questionTimeout(question,seconds = 30): #removes a question if it hasn't been answered after "seconds" seconds.
-    await asyncio.sleep(seconds) #waits for "seconds" seconds (pls note using the asyncio.sleep, not time.sleep, dont make that mistake)
-    if question in currentQuestions: #checkes if the question is in current questions (a question will be removed when it is answered)
-        reply = question.originalMessage.author.name + " took too long to answer." #gets the new text that the message needs to be updated to.
-        embed = question.getTimedOutEmbed() #gets the embed that will be used for a timedout message.
-        await client.edit_message(question.message,reply,embed = embed)
-        await client.clear_reactions(question.message)
-        currentQuestions.remove(question)
-
+# This lets ask.py have access to the client variable
+# needed for sending messages and stuff in ask.py
+ask.init(client)
 
 @client.event
 async def on_ready():
@@ -30,64 +16,53 @@ async def on_ready():
 async def on_message(message):
     #print(message.author,message.content)
     if message.author == client.user: #if the message was sent by the bot
-        if len(message.embeds) > 0: #check for any embeds
-            embed = message.embeds[0] #gets the first embed (how do you even send a message with multiple embeds)
-            for question in unassignedQuestions: # for each question that has not been paired with a message.
-                print(embed['title'][2:-2]) #we can ignore this dude right here.
-                if  embed['title'][2:-2] == question.question and message.content == question.originalMessage.author.mention: #detects if the message should be paired with the question
-                                                                                                                              #by comparing the embed title (without bold formatting) with the question object
-                                                                                                                              #and by comparing the message itself (which should just be a mention) to a mention of the user
-                    question.setMessage(message) #assigns the question object it's message.
-                    for reaction in question.reactions: #for every suitable reaction
-                        await client.add_reaction(question.message, reaction) #add the reaction to the message
-                    currentQuestions.append(question) #add the question to current qusetions
-                    unassignedQuestions.remove(question) #and remove it from unassinged question
+        # we aren't doing anything here... for now. :thonk:
+        return
 
     else: # if the message is not sent by the bot (could still be send by another bot, this is fine, I want to make a bot to play this)
         msg = str(message.content).lower() #store the content of the message (lower case.)
-    	
-        if msg == "start new farm": #selfexplanitory
-            question = "Are you sure you wish to start a new farm?"
-            unassignedQuestions.append(ask.question(message,question)) # creates a question object, stick it in unassinged questions
-            question = unassignedQuestions[-1] #grabs the question
-            embed = unassignedQuestions[-1].getEmbed() #creates the embed for the question
-            channel = message.channel
-            reply = message.author.mention
-            await client.send_message(channel,reply,embed = embed) #sends the message
-            await questionTimeout(question) #checks if the question is answered after 30 seconds and removes it otherwise.
-            
+
+        if msg == "start new farm":
+            # This is a simple example showing how to use the new ask.ask() command.
+            answer = await ask.ask(message, "Are you sure you wish to start a new farm?")
+            if answer == True:
+                print("answered with thumbs up")
+            elif answer == False:
+                print("answered with thumbs down")
+            elif answer == None:
+                print("timed out and did not answer")
+
+            # another example: (which I encourage you to try out)
+            #await ask.ask(message, "You really shouldn't react with a pig", answers={"🐷":"pig","🐮":"cow"}, timeout=10)
+            # which will return "pig" if the user reacts with the pig, "cow" if the user reacts with the cow,
+            # and None if the user didn't respond within 10 seconds
 
         if msg == "hey": # this is just what I use to get some dubug info
             await client.send_message(message.channel, "hey")
-            print(currentQuestions)
+            print(questions)
 
 
 @client.event
 async def on_reaction_add(reaction, user): #runs whenever a reaction is added.
+    message = reaction.message
     '''
     for message in client.messages:
         print(message)
     '''
-    if user != client.user: #we want to ignore the bots reactions
-        for question in currentQuestions: #for all the question that need to be answered
-            checkCount = 10 #how many messages to check
-            if checkCount > len(client.messages): #makes sure we're not trying to check messages we dont have. 
-                checkCount = len(client.messages)
-            messagei = 0
-            while messagei > -checkCount: #checkes the last "checkCount" (10) messages sent.
-                messagei -= 1
-                if question.message == client.messages[messagei]: #check if the message is assinged to the question (why did I do it this way)
-                    if question.originalMessage.author == user: #checks if the person answering the message is the person who was asked the question.
-                        #await client.send_message(question.message.channel,"you reacted to the question.") 
-                        embed = question.getAnsweredEmbed() #gets the new embed
-                        reply = question.originalMessage.author.name + " answered with: " + reaction.emoji
-                        await client.edit_message(question.message,reply,embed = embed)
-                        await client.clear_reactions(question.message)
-                        currentQuestions.remove(question) #removes the question, we dont need him anymore.
-                    else:
-                        pass #if the person answering the question was the wrong person then DONT send a message telling them that (lets not spam the channel).
-                        await client.send_message(question.message.channel, user.mention + " this question isn't for you.")
-                    break
+    if user != client.user:
+        for question in ask.questions:
+            if question.message.id == message.id:
+                # The message reacted to is a question (that hasn't been answered already cause it's in ask.questions)
+                if user == question.origMessage.author:
+                    # And the right person has reacted too it!
+                    for emoji in question.answers:
+                        if emoji == reaction.emoji:
+                            # The correct user has reacted to a question with a valid emoji
+                            question.emoji = emoji
+                            question.answer = question.answers.get(emoji)
+                            question.answered = True
+                            return
+
 
 # Will try and get a token from code/token.txt
 # If this fails (file does not exist) then it asks for the token and creates the file
@@ -101,7 +76,6 @@ except OSError:
     client.run(token)
 else:
     token = file.read()
-    print(token) #why are we printing the token here? It serves no purpose.
     file.close()
     client.run(token)
 
