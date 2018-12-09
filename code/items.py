12faa43a25fdea26041313e0181cab6baaa70723
Uni-tato <alex.lasenby@yahoo.co.nz>
2018-12-10 12:17:13 +1300

    added items.py

--- code/items.py
+++ code/items.py
@@ -10,14 +10,28 @@ class Container:
         self.name = name
         self.items = items
 
-    def append(items):
+    def append(self, items):
         if type(items) == Container:
-            items = items.items
+            items = items.items # Allows easier addition of 2 containers eg. Container1.append(Container2).
         for item in items:
             if item in self.items:
                 self.items[item] += items[item]
             else:
                 self.items[item] = items[item]
+
+    def remove(self, items):
+        if type(items) == Container:
+            items = items.items
+        for item in items:
+            if item not in self.items: # return an error if the container does not have (enough of) an item.
+                raise valueError(f"Item: \"{item}\" not present in Container.")
+            elif items[item] > self.items[item]:
+                raise valueError(f"More \"{item}\"s removed than in Container.")
+            
+            elif items[item] = self.items[item]:
+                del self.items[item]
+            else:
+                self.items[item] -= items[item]
 
 
 items = []
