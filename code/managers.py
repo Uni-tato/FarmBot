fb960d9aad8a8d3ea062cd7c300321d238c95f15
Hoboneer <gabriel.lisaca@gmail.com>
2018-12-13 15:17:05 +1300

    Implement basic methods for `*Manager` classes
    The `get_type` method in `CropManager` is there as `Crop`s and `Tree`s
    are to be treated with a subclass relationship, i.e., `Tree` will be a
    subclass of `Crop`.

--- code/managers.py
+++ code/managers.py
@@ -1,3 +1,8 @@
+from util import FarmbotCSVDialect
+
+import csv
+
+
 # TODO: Figure out what methods should be here. Flesh out the events mechanic more.
 class EventManager:
     pass
@@ -6,39 +11,81 @@ class EventManager:
 # Crops and trees are handled by this class.
 class CropManager:
     def __init__(self, crops_file_text):
-        # TODO: Implement a general version of the csv parsing.
         self.crops = {}
-    
+        # TODO: Implement a general version of the csv parsing.
+        reader = csv.DictReader(
+            (row for row in crops_file if not row.startswith("#")),
+            dialect=FarmbotCSVDialect,
+        )
+        for row in reader:
+            self.crops[row["name"]] = {
+                "seed": row["seed"],
+                "item": row["item"],
+                "minItem": int(row["minItem"]),
+                "maxItem": int(row["maxItem"]),
+                "minLifeTime": int(row["minLifeTime"]),
+                "maxLifetime": int(row["maxLifetime"]),
+                "emoji": row["emoji"],
+            }
+
+    def _get_inner(self, crop, field):
+        try:
+            crop_info = self.crops[crop]
+        except KeyError:
+            # TODO: Replace with a non-user-facing error?
+            raise ValueError(f"`{crop}` is not a valid crop.")
+
+        return crop_info[field]
+
     def get_min_items(self, crop):
-        ...
+        return _get_inner(crop, "minItem")
 
     def get_max_items(self, crop):
-        ...
+        return _get_inner(crop, "maxItem")
 
     def get_min_lifetime(self, crop):
-        ...
+        return _get_inner(crop, "minLifeTime")
 
     def get_max_lifetime(self, crop):
-        ...
+        return _get_inner(crop, "minItem")
 
     def get_emoji(self, crop):
-        ...
+        return _get_inner(crop, "minItem")
 
     # Returns whether it is a `Crop` or `Tree`.
     def get_type(self, crop):
-        ...
+        return _get_inner(crop, "minItem")
 
 
 class MarketManager:
     def __init__(self, items_file_text):
-        # TODO: Implement a general version of the csv parsing.
         self.items = {}
+        # TODO: Implement a general version of the csv parsing.
+        reader = csv.DictReader(
+            (row for row in item_file if not row.startswith("#")),
+            dialect=FarmbotCSVDialect,
+        )
+        for row in reader:
+            self.items[row["name"]] = {
+                "buy": row["buy"],
+                "sell": row["sell"],
+                "emoji": row["emoji"],
+            }
+
+    def _get_inner(self, item, field):
+        try:
+            item_info = self.items[item]
+        except KeyError:
+            # TODO: Replace with a non-user-facing error?
+            raise ValueError(f"`{item}` is not a valid item.")
+
+        return item_info[field]
 
     def get_buy_price(self, item_name):
-        ...
+        return _get_inner(item_name, "buy")
 
     def get_sell_price(self, item_name):
-        ...
+        return _get_inner(item_name, "sell")
 
     def get_emoji(self, item_name):
-        ...
+        return _get_inner(item_name, "emoji")
