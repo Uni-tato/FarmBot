b1a52bfe531f4bb8d7fb942f8405189e42c07726
Hoboneer <gabriel.lisaca@gmail.com>
2018-12-22 22:11:46 +1300

    Add module docstring for `ask.py`

--- code/ask.py
+++ code/ask.py
@@ -1,3 +1,4 @@
+"""Handle questions asked to player."""
 # another example: (which I encourage you to try out)
 # await ask.ask(message, "You really shouldn't react with a pig", answers={"🐷":"pig","🐮":"cow"}, timeout=10)
 # which will return "pig" if the user reacts with the pig, "cow" if the user reacts with the cow,
